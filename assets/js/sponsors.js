0d53eac4e6861c60a0d401dc379802e7b2492224
Santiago Moreno <rosamodu@gmail.com>
2018-03-21 17:16:01 -0500

    Sponsors page created with Dynamic Resources

---
+++ assets/js/sponsors.js
@@ -0,0 +1,53 @@
+	// Use of this Script Requires the Tabletop.js Library. The Calling HTML File must include tabletop.js
+
+// Address of the Google Sheets Database
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1piWHKU72s6uuq-8cGROitlvd3rZGqzWPwZfPPbz_VAE/edit?usp=sharing';
+
+function init() {
+Tabletop.init( { key: public_spreadsheet_url,
+                 callback: showInfo,
+                 simpleSheet: true } );
+}
+
+window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
+
+// Method that gets called when data has been pulled from Google Sheets
+function showInfo(data) {
+	var sponsorName = [];
+	var sponsorDate = [];
+	var sponsorDescription = [];
+	var sponsorContact = [];
+	var index = 0; // Line 2 is index 0 
+	var htmlContent = "";
+	//Builds the Forms and Addresses arrays using the Google Sheets Data
+	//  var[index] = data[index]<COL_NAME>
+	while (data[index] != null) {
+		sponsorName[index] = data[index].Sponsor_Name;
+		sponsorDate[index] = data[index].Sponsor_Date;
+		sponsorDescription[index] = data[index].Sponsor_Description;
+		sponsorContact[index] = data[index].Sponsor_Contact;
+
+
+		// Writes HTML code based on Form responses
+		// alert(webURL[index] == undefined);
+		// link = "";
+		// link = webURL[index];
+
+		htmlContent += '<h3><b>'
+		+ sponsorName[index]
+		+ '</h3></b><h4>Sponsor since '
+		+ sponsorDate[index]
+		+ '</h4><p>'
+		+ sponsorDescription[index]
+		+ '</p><a href="'
+		+ sponsorContact[index]
+		+ '" class="button special">Learn More About '
+		+ sponsorName[index]
+		+ '</a><br><br><br><br>';
+
+
+		index++;
+
+	}
+	document.getElementById("htmlContentContainer").innerHTML = htmlContent;
+}

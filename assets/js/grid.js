48451beee6695488767a3ca7dca9d07eccf93908
rolsanmordua <rosamodu@gmail.com>
2018-10-21 20:42:47 -0500

    Updated hardcoded projects...
    Temporary solution since we are busier this semester, figure out how to reintroduce dynamically

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -514,3 +514,74 @@ var Grid = (function() {
 	};
 
 })();
+
+
+
+
+
+
+
+
+// Use of this Script Requires the Tabletop.js Library. The Calling HTML File must include tabletop.js
+
+// Address of the Google Sheets Database
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1FrkMGTyXJ-MWYYN_jws3xffMbAojz77rBRE4i3RamUc/edit?usp=sharing';
+
+function init() {
+Tabletop.init( { key: public_spreadsheet_url,
+							 callback: showInfo,
+							 simpleSheet: true } );
+}
+
+window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
+
+// Method that gets called when data has been pulled from Google Sheets
+function showInfo(data) {
+var projLogo = [];
+var projTitle = [];
+var devpostURL = [];
+var madeFor = [];
+var developers = [];
+var hackathonURL = [];
+var commentary = [];
+var commentary2 = [];
+var videoDemo = [];
+var index = 0; // Line 2 is index 0
+var htmlContent = '';
+//Builds the Forms and Addresses arrays using the Google Sheets Data
+//  var[index] = data[index]<COL_NAME>
+while (data[index] != null) {
+	projLogo[index] = data[index].Project_Logo;
+	projTitle[index] = data[index].Project_Title;
+	devpostURL[index] = data[index].Devpost_URL;
+	madeFor[index] = data[index].made_for_Hackathon;
+	developers[index] = data[index].Developers;
+	hackathonURL[index] = data[index].Hackathon_URL;
+	commentary[index] = data[index].Commentary;
+	commentary2[index] = data[index].Commentary2;
+	videoDemo[index] = data[index].Video_Demo;
+
+
+		htmlContent += '<li><a href="'
+			+ devpostURL[index]
+			+ '" target="_blank" data-largesrc="'
+			+ projLogo[index]
+			+ '" data-title="'
+			+ projTitle[index]
+			+ '" data-description="'
+			+ commentary[index]
+			+ '"><img src="'
+			+ projLogo[index]
+			+ '" class="projects" alt="'
+			+ projTitle[index]
+			+ '"/>';
+				index++;
+		}
+		// htmlContent += '</ol></div>';
+
+// htmlContent += '</ol></div>';
+document.getElementById("htmlContentContainer").innerHTML = htmlContent;
+
+
+
+}

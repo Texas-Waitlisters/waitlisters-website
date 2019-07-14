ea4ff473808198f6eb74bbd2b37002819df17420
rolsanmordua <rosamodu@gmail.com>
2019-07-14 00:42:02 -0700

    Revising layout

--- assets/js/hackathons.js
+++ assets/js/hackathons.js
@@ -14,39 +14,72 @@ window.addEventListener('DOMContentLoaded', init)	// Calls method init when Shee
 // Method that gets called when data has been pulled from Google Sheets
 function showInfo(data) {
 	var hackathonName = [];
+  var hackathonPhoto = [];
 	var projects = [];
+  var awards = [];
 	var developers = [];
 	var hackathonURL = [];
   var hackathonReview = [];
 	var index = 0; // Line 2 is index 0
 	var htmlContent = '';
+  var projectsContent = "";
+  var awardsContent = "";
 	//Builds the Forms and Addresses arrays using the Google Sheets Data
 	//  var[index] = data[index]<COL_NAME>
 	while (data[index] != null) {
 		hackathonName[index] = data[index].Hackathon_Name;
+    hackathonPhoto[index] = data[index].Hackathon_Photo;
 		projects[index] = data[index].Projects;
+    awards[index] = data[index].Awards;
 		developers[index] = data[index].Developers;
 		hackathonURL[index] = data[index].Hackathon_URL;
     hackathonReview[index] = data[index].Hackathon_Review;
 
+    // first row
+    if (index == 0) {
       htmlContent += '<a href="'
         + hackathonURL[index] + '"><h2>'
         + hackathonName[index]
         + '</h2></a>'
-        + '<h3><b>Projects: </b></h3>'
-        + '<p>' + projects[index] + '</p>'
-        + '<h3><b>Developers</b></h3>'
-        + '<p>' + developers[index] + '</p>'
-        + '<h3><b>Hackathon Review</b></h3>'
-        + hackathonReview[index]
-        + '<br><br><br>';
-          index++;
+        + '<h3><b>Project(s)</b></h3><p>'
+        + projects[index] + ' ('
+        + developers[index] + ')</p>';
+      if (awards[index].length > 0) {
+        htmlContent += '<h3><b>Awards </b></h3>'
+        + '<p>' + awards[index] + ' (' + projects[index] + ')</p>';
       }
-      // htmlContent += '</ol></div>';
+        // htmlContent += '<br><br>'
+    // further rows
+    } else if (index > 0) {
+      if (hackathonName[index] != hackathonName[index-1]) {
+        htmlContent += '<a href="'
+          + hackathonURL[index] + '"><h2>'
+          + hackathonName[index]
+          + '</h2></a>'
+          + '<h3><b>Project(s)</b></h3><p>'
+          + projects[index] + ' ('
+          + developers[index] + ')</p>';
+          if (awards[index].length > 0) {
+            htmlContent += '<h3><b>Awards </b></h3>'
+            + '<p>' + awards[index] + ' (' + projects[index] + ')</p>';
+          }
+      } else if (hackathonName[index] == hackathonName[index-1]) {
+        htmlContent += '<p>' + projects[index] + ' ('
+          + developers[index] + ')</p>';
+          if (awards[index].length > 0) {
+            htmlContent += '<p>' + awards[index] + ' (' + projects[index] + ')</p>';
+          }
+      }
+      // htmlContent += '<br><br>'
+    }
 
-  // htmlContent += '</ol></div>';
-	document.getElementById("htmlContentContainer").innerHTML = htmlContent;
-
-
+    // projectsContent = "";
+    // awardsContent = "";
+    htmlContent += '<br><br>'
+    index++;
+      htmlContent += '</ol></div>';
+  }
+  // htmlContent += '<br></br>';
+  document.getElementById("htmlContentContainer").innerHTML = htmlContent;
 
 }
